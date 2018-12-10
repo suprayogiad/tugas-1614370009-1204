9dad8a774087569f9f769fd7bd9c49d29d5d08e7
Supra Yogi Adnan <45608283+suprayogiad@users.noreply.github.com>
2018-12-10 20:37:30 +0700

    Add files via upload

---
+++ famili.pl
@@ -0,0 +1,10 @@
+(rita, abdul).
+(rita, rudi).
+(husin, rudi).
+(rudi, tuti).
+(rudi, siti).
+(siti, amir).
+(siti, basir).
+(a) orang tua(siti, basir).
+(b) nenek(rita, siti).
+(c) anak(tuti, -).
---
+++ king.pl
@@ -0,0 +1,10 @@
+anak(Masih dicari, bahrun).
+anak(Masih dicari, farah).
+anak(bahrun, fahri).
+anak(fahri, salsa).
+anak(fahri, arfan).
+anak(salma, farhan).
+anak(farah, burhan).
+anak(farah, ferdi).
+anak(ferdi, syifa).
+anak(ferdi, fariz).
